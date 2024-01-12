965b630f75ee194aa3713d1c45c61e5a7cab7769
Eric Bellezza <erichernan27@hotmail.com>
2024-01-11 23:53:42 -0300

    carpeta componentes agregada

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import './components/boton.jsx';
+import MyButton from './components/boton.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <MyButton/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/boton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MyButton() {
+    return (
+      <button className="my-button">Soy un botón</button>
+    );
+  }
+
+export default MyButton;
