bfa1978c4156f36d7de0b7670ea11179abdcb9fd
Eric Bellezza <erichernan27@hotmail.com>
2024-01-12 17:02:03 -0300

    Boton Square ahora marca 'X'

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function Square() {
+  let [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+    console.log("Click");
+  }
     return (
-        <button className="square">1</button>
+        <button className="square" onClick={handleClick}> {value} </button>
     );
   }
