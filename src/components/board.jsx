71836d3c419f866ddb1d3a48ab8038422a95e025
Eric Bellezza <erichernan27@hotmail.com>
2024-01-14 13:06:38 -0300

    add function to set status winner

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -6,7 +6,38 @@ export default function Board() {
     let [xIsNext, setXIsNext] = useState(true);
     let [squares, setSquares] = useState(Array(9).fill(null));
 
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = "Ganador: " + winner;
+    } else {
+      status = "Siguiente jugador: " + (xIsNext ? "X" : "O");
+    }
+
+    function calculateWinner(squares) {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],  
+        [2, 4, 6]
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+          return squares[a];
+        }
+      }
+      return null;
+    }
+
     function handleClick(i) {
+      if (squares[i] || calculateWinner(squares)) {
+        return;
+      }
       const nextSquares = squares.slice();
       if (squares[i]) {
         return;
@@ -23,6 +54,7 @@ export default function Board() {
 
     return (
       <>
+        <div className="status">{status}</div>
         <div className="board-row">
           <Square value={squares[0]} onClickSquare={() => handleClick(0)} />
           <Square value={squares[1]} onClickSquare={() => handleClick(1)} />
